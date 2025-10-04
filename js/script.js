703610bdaf6d4116e4626227e403863d7466a395
ChrisBegines123 <ChristopherBegines@gmail.com>
2025-10-04 16:45:07 -0400

    chnaged website

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
+// Fade-in effect
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("fade-in");
-  // You could also add smooth scroll behavior
-  const links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(link => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const target = document.querySelector(link.getAttribute("href"));
-      if (target) {
-        target.scrollIntoView({ behavior: "smooth" });
-      }
-    });
-  });
 });
